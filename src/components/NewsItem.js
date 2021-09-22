a52a1fbb9376f1bbb0d585fb195ecbfc23b91802
Tawanjot Singh <54388983+tawan21@users.noreply.github.com>
2021-09-22 17:41:11 +0530

    Day #2

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={imageUrl?imageUrl:"https://tapinto-production.s3.amazonaws.com/uploads/articles/br/best_crop_1936b17b9719e6f57ca8_breaking_news.jpg"} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
